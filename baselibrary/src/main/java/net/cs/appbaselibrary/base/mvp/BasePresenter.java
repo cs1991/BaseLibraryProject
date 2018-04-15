1b4dec5b94371a110e3515da1cc980c42a32e805
cs1991 <562387089@qq.com>
2018-04-15 14:53:09 +0800

    提交基础代码

--- baselibrary/src/main/java/net/cs/appbaselibrary/base/mvp/BasePresenter.java
+++ baselibrary/src/main/java/net/cs/appbaselibrary/base/mvp/BasePresenter.java
@@ -6,6 +6,7 @@ import java.lang.ref.Reference;
 import java.lang.ref.WeakReference;
 
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
 
 /**
  * Created by lenovo on 2017/1/4.
@@ -14,7 +15,7 @@ import io.reactivex.disposables.CompositeDisposable;
 public abstract class BasePresenter<V extends MvpView> {
     protected Reference<V> mViewRef;//View 接口类型的弱引用
     @NonNull
-    protected CompositeDisposable disposables = new CompositeDisposable();
+    protected CompositeDisposable disposables;
 
     protected abstract void initPresenter();
     public void attachView(MvpView view) {
@@ -39,6 +40,8 @@ public abstract class BasePresenter<V extends MvpView> {
             mViewRef.clear();
             mViewRef = null;
         }
+        unSubscribe();
+        disposables = null;
         onDestroy();
     }
 
@@ -54,7 +57,12 @@ public abstract class BasePresenter<V extends MvpView> {
             disposables.clear();
         }
     }
-
+    public void addDispose(Disposable disposable) {
+        if (disposables == null) {
+            disposables = new CompositeDisposable();
+        }
+        disposables.add(disposable);//将所有 Disposable 放入集中处理
+    }
     /**
      * 为测试而写的方法，无需做任何操作
      * @return
